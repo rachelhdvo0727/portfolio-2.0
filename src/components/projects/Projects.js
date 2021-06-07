a3afdd374dca485cd6c1154db065dc32e9b83cde
rachelvo <rachelhd.vo@outlook.com>
2021-06-07 14:56:44 +0200

    used local json

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -9,63 +9,53 @@ import ProjectBtn from "../project-btn/ProjectBtn";
 import LoadingSkeleton from "../loading-skeleton/LoadingSkeleton";
 
 function Projects() {
-  const [projects, setProjects] = useState([]);
-  const history = useHistory();
+	const [projects, setProjects] = useState([]);
+	const history = useHistory();
+	// GET projects
+	useEffect(() => {
+		axios
+			.get("../../db/projects.json")
+			.catch((error) => console.error(error))
+			.then((data) => {
+				setInterval(() => {
+					setProjects(data.data);
+				}, 800);
+			});
+	}, []);
 
-  // GET projects
-  useEffect(() => {
-    axios
-      .get(
-        "https://portfolio-3dd4.restdb.io/rest/projects?q={}&sort=timeOfComplete&dir=-1",
-        {
-          headers: {
-            "x-apikey": "6004555c1346a1524ff12b73",
-          },
-        }
-      )
-      .catch((error) => console.error(error))
-      .then((res) => {
-        setInterval(() => {
-          setProjects(res.data);
-        }, 800);
-      });
-  }, []);
+	const clickOneProject = (thisproject, i, e) => {
+		e.currentTarget.classList.toggle("pressed");
 
-  const clickOneProject = (thisproject, i, e) => {
-    e.currentTarget.classList.toggle("pressed");
+		if (e.currentTarget.classList.contains("pressed")) {
+			setTimeout(() => {
+				history.push(`/project/${thisproject._id}`, { project: thisproject });
+			}, 500);
+		}
+	};
 
-    if (e.currentTarget.classList.contains("pressed")) {
-      setTimeout(() => {
-        history.push(`/project/${thisproject._id}`, { project: thisproject });
-      }, 500);
-    }
-  };
-
-  const isSmallestScreen = useMediaQuery({ query: "(max-width: 340px)" });
-
-  return (
-    <main className={styles.projectsPage + " fadeIn"}>
-      <h1 className={isSmallestScreen ? "display-2 " : "display-1 "}>
-        Projects
-      </h1>
-      <section className={styles.allProjectsWrapper}>
-        {(projects.length > 0 || projects) &&
-          projects.map((project, i) => (
-            <ProjectBtn
-              key={i}
-              projectName={project.projectName}
-              projectImg={
-                "https://portfolio-3dd4.restdb.io/media/" +
-                project.projectThumbnail[0]
-              }
-              onClick={(e) => {
-                clickOneProject(project, i, e);
-              }}
-            />
-          ))}
-        {(projects.length <= 0 || !projects) && <LoadingSkeleton />}
-      </section>
-    </main>
-  );
+	const isSmallestScreen = useMediaQuery({ query: "(max-width: 340px)" });
+	return (
+		<main className={styles.projectsPage + " fadeIn"}>
+			<h1 className={isSmallestScreen ? "display-2 " : "display-1 "}>
+				Projects
+			</h1>
+			<section className={styles.allProjectsWrapper}>
+				{(projects.length > 0 || projects) &&
+					projects.map((project, i) => (
+						<ProjectBtn
+							key={i}
+							projectName={project.projectName}
+							projectImg={
+								"../../images/" + project.projectThumbnail[0] + ".jpg"
+							}
+							onClick={(e) => {
+								clickOneProject(project, i, e);
+							}}
+						/>
+					))}
+				{(projects.length <= 0 || !projects) && <LoadingSkeleton />}
+			</section>
+		</main>
+	);
 }
 export default Projects;
